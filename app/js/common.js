0981146ad2a0c3bb20209b6f4f91dbbcf7ab1e06
bangwww <bang.1234@mail.ru>
2016-09-24 15:16:42 +0300

    add transition's animation

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
 
-	$(".btn_dwn").click(function(){
-
-		//$("html,body").animate({scrollTop:$('header').height()},"slow");
-
+	$(".btn_dwn, .btn_up").click(function(){
 		$('header, .bg_box').css('min-height', '0').slideToggle(1000);
+		$('.s_main').slideToggle(1000, function() {
+			$(".btn_dwn, .btn_up").slideToggle(1000);
+		});
 	});
+
 
 	function heightDetect() {
 		$("header, .bg_box").css("height", $(window).height());
