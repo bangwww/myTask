3dae8c97e7e14e7cffbf6904191f803f72ff0cc3
bangwww <bang.1234@mail.ru>
2016-09-24 17:08:50 +0300

    add fonts and user's filter

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	$(".btn_dwn, .btn_up").click(function(){
-		$('header, .bg_box').css('min-height', '0').slideToggle(1000);
+		$('header, .bg_box').slideToggle(1000);
 		$('.s_main').slideToggle(1000, function() {
 			$(".btn_dwn, .btn_up").slideToggle(1000);
 		});
